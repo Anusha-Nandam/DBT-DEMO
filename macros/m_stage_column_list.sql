b24079c896415181be4df06696aba7c3d1e3fd72
siva mohan <siva.mohan@anblicks.com>
2024-05-15 10:31:28 +0000

    COMPLETED CUSTOMERS MODEL IN MAIN SCHEMA

---
+++ macros/m_stage_column_list.sql
@@ -0,0 +1,28 @@
+{% macro m_stage_column_list(table_schema,TABLE_NAME) %}
+ 
+    {% set src_program_query %}
+        WITH COL_CTE AS
+        (
+ 
+            select
+            COLUMN_NAME AS COLUMN_STRING
+            from information_schema.columns
+            where TABLE_NAME='{{TABLE_NAME}}' and table_schema='{{table_schema}}'
+        )  
+        SELECT COLUMN_STRING  FROM COL_CTE
+    {% endset %}
+ 
+    {% set src_results = run_query(src_program_query) %}
+ 
+    {% if execute %}
+        {% set src_results_list = src_results.columns[0].values() %}
+    {% endif %}
+ 
+    {% set src_selection_list = [] %}
+        {% for i in src_results_list %}
+            {% do src_selection_list.append(i) %}
+        {% endfor %}
+   
+    {{return(src_selection_list)}}
+ 
+{% endmacro %}
---
+++ models/HUBS/HUB_CUST.sql
@@ -0,0 +1,8 @@
+{%- set source_model = ["STG_CUSTOMERS"] -%}
+{%- set src_pk = "CUSTOMER_ID" -%}
+{%- set src_nk = "FIRST_NAME" -%}
+{%- set src_ldts = "FIRST_ORDER" -%}
+{%- set src_source = "LAST_NAME" -%}
+
+{{ automate_dv.hub(src_pk=src_pk, src_nk=src_nk, src_ldts=src_ldts,
+                  src_source=src_source, source_model=source_model) }}
