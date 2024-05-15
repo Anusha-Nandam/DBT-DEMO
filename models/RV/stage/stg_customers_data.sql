582d70b7c8c710bb9a6aa0264e1b9866ab4a2069
Anusha Nandam <anusha.nandam@anblicks.com>
2024-05-15 08:37:23 +0000

    added derived columns to stage

--- models/RV/stage/stg_customers_data.sql
+++ models/RV/stage/stg_customers_data.sql
@@ -2,6 +2,11 @@
 
 {% set yaml_metadata%}
 source_model : 'raw_customers_data'
+derived_columns:
+  RECORD_LOAD_DATE: 'RECORD_LOAD_DATE'
+  RECORD_START_DATE : 'RECORD_START_DATE'
+  RECORD_END_DATE : 'RECORD_END_DATE'
+  RECORD_IS_ACTIVE : 'RECORD_IS_ACTIVE'
 hashed_columns :
     CUST_ID : ['ID']
 {% endset %}
@@ -10,11 +15,13 @@ hashed_columns :
 {% set metadata_dict = fromyaml(yaml_metadata) %}
  
 {% set source_model = metadata_dict['source_model'] %}
+
+{% set derived_columns = metadata_dict['derived_columns'] %}
  
 {% set hashed_columns = metadata_dict['hashed_columns'] %}
 
 WITH staging AS (
-{{ automate_dv.stage(include_source_columns=true,source_model=source_model,
+{{ automate_dv.stage(include_source_columns=true,source_model=source_model,derived_columns=derived_columns,
                   hashed_columns=hashed_columns) }} )
  
 SELECT * FROM staging
